f69ed7ee6756238c9dea875f51a8b11946de761e
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-08-09 02:52:34 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import './App.css';
 import Navbar from './components/Navbar';
 import ListSongs from './components/ListSongs';
 import DetailSong from './components/DetailSong';
+import Playing from './components/Playing';
+import { Songs } from './Context';
+import DataSongs from "./data/songs.json";
+import { useState } from 'react';
 
 function App() {
+  const [song, setSong] = useState(DataSongs[0]);
+  const handleSetSong = (idsong) => {
+    const song = DataSongs.find(song => song.id === idsong);
+    if(!song)
+      setSong(DataSongs[0]);
+    else
+      setSong(song);
+  }
   return (
-    <div className=""> 
-      <Navbar />
-      <div className='grid grid-cols-3 bg-gray-700 h-screen-navbar-player'>
-        {/*span 1*/}
-        <DetailSong />
-        {/*span 2*/}
-        <ListSongs />
-      </div> 
+    <div className="App"> 
+      <Songs.Provider value={{ DataSongs, song, handleSetSong }}>
+        <Navbar />
+        <div className='grid grid-cols-3 bg-gray-700 h-screen-navbar-player overflow-hidden'>
+          {/*span 1*/}
+          <DetailSong />
+          {/*span 2*/}
+          <ListSongs />
+        </div> 
+        <Playing/>
+      </Songs.Provider>
     </div>
   );
 }
--- src/components/ListSongs.js
+++ src/components/ListSongs.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Songs } from "../Context";
 
 export default function ListSongs() {
+    const {DataSongs, handleSetSong, song} = useContext(Songs);
+    const [idSong, setIdSong] = useState(0);
+    const handlePlaySong = (idsong) =>  {
+        setIdSong(idsong);
+        handleSetSong(idsong);
+    }
+    useEffect(()=>{
+        setIdSong(song.id);
+    },[song])
     return(
-        <div className="col-span-2">
-            <table className="table-auto w-full mt-3">
+        <div className="col-span-2 overflow-y-scroll">
+            <table className="table-auto w-full mt-1">
                 <thead className="text-white h-12">
                     <tr>
-                        <th className="w-[10%]">#</th>
+                        <th className="w-[10%]">STT</th>
                         <th className="text-left">title</th>
-                        <th className="w-[10%]">author</th>
+                        <th className="w-[10%]">Author</th>
                         <th className="w-[10%]">Download</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td className="text-center">1</td>
-                        <td>1</td>
-                        <td className="text-center">1</td>
-                        <td className="text-center"><i className="fa fa-download"></i></td>
+                    { DataSongs.map((song, index) => (
+                    <tr key={index} 
+                        className={`bg-slate-800 h-12 text-gray-400 hover:bg-slate-600 hover:text-cyan-600 cursor-pointer ${idSong === song.id && `bg-slate-600 text-cyan-600`}`} 
+                        onClick={()=>handlePlaySong(song.id)}
+                    >
+                        <td className="text-center">{index + 1}</td>
+                        <td>{song.name}</td>
+                        <td className="text-center">{song.author}</td>
+                        <td className="text-center"><a href={song.url}><i className="fa fa-download"></i></a></td>
                     </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
